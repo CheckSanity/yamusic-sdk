2fdfdb8d6b5b07e8c174e215715cda38c517c7ba
Garik Bezruchko <garik.bezruchko@yandex.ru>
2023-08-03 10:12:19 +0300

    New API endpoints

--- src/artists/Artists.api.ts
+++ src/artists/Artists.api.ts
@@ -1,5 +1,5 @@
 import { BaseApi } from '../BaseApi';
-import { ArtistDetailed } from './Artists.types';
+import { ArtistAlbums, ArtistDetailed, ArtistTracks } from './Artists.types';
 
 export class ArtistsApi extends BaseApi {
   public artist(uid: number): Promise<ArtistDetailed> {
@@ -8,5 +8,13 @@ export class ArtistsApi extends BaseApi {
 
   public artists(uid: number[]): Promise<ArtistDetailed[]> {
     return this.getRequest<ArtistDetailed[]>(`artists?artistIds=${uid}`);
+  }
+
+  public artistTracks(uid: number): Promise<ArtistTracks> {
+    return this.getRequest<ArtistTracks>(`artists/${uid}/tracks`);
+  }
+
+  public artistAlbums(uid: number): Promise<ArtistAlbums> {
+    return this.getRequest<ArtistAlbums>(`artists/${uid}/direct-albums`);
   }
 }
--- src/search/Search.api.spec.ts
+++ src/search/Search.api.spec.ts
@@ -1,4 +1,4 @@
-import { YaMusicSDK } from '../YaMusicSDK';
+import { Artist, YaMusicSDK } from '../YaMusicSDK';
 import {
   buildIntegrationTestSdkInstance,
   FetchApiSpy,
@@ -12,75 +12,91 @@ describe('Search API', () => {
     [sdk, fetchSpy] = buildIntegrationTestSdkInstance();
   });
 
-  it('search artist should return artists', async () => {
-    const result = await sdk.search.search({
-      text: 'Kasabian',
-      type: 'artist',
-      page: 1,
+  describe('search', () => {
+    it('search artist should return artists', async () => {
+      const result = await sdk.search.search({
+        text: 'Kasabian',
+        type: 'artist',
+        page: 1,
+      });
+
+      expect(fetchSpy.request().input).toBe(
+        `https://api.music.yandex.net/search?text=Kasabian&type=artist&page=1`,
+      );
+
+      expect(result.type).toStrictEqual('artist');
+      expect(result.page).toStrictEqual(1);
+      expect(result.text).toStrictEqual('Kasabian');
+      expect(result.artists).toBeDefined();
+      expect(result.artists?.results.length).toBeGreaterThan(0);
     });
 
-    expect(fetchSpy.request().input).toBe(
-      `https://api.music.yandex.net/search?text=Kasabian&type=artist&page=1`,
-    );
+    it('search track should return tracks', async () => {
+      const result = await sdk.search.search({
+        text: 'underdog',
+        type: 'track',
+        page: 1,
+      });
 
-    expect(result.type).toStrictEqual('artist');
-    expect(result.page).toStrictEqual(1);
-    expect(result.text).toStrictEqual('Kasabian');
-    expect(result.artists).toBeDefined();
-    expect(result.artists?.results.length).toBeGreaterThan(0);
+      expect(fetchSpy.request().input).toBe(
+        `https://api.music.yandex.net/search?text=underdog&type=track&page=1`,
+      );
+
+      expect(result.type).toStrictEqual('track');
+      expect(result.page).toStrictEqual(1);
+      expect(result.text).toStrictEqual('underdog');
+      expect(result.tracks).toBeDefined();
+      expect(result.tracks?.results.length).toBeGreaterThan(0);
+    });
+
+    it('search playlist should return playlists', async () => {
+      const result = await sdk.search.search({
+        text: 'house',
+        type: 'playlist',
+        page: 1,
+      });
+
+      expect(fetchSpy.request().input).toBe(
+        `https://api.music.yandex.net/search?text=house&type=playlist&page=1`,
+      );
+
+      expect(result.type).toStrictEqual('playlist');
+      expect(result.page).toStrictEqual(1);
+      expect(result.text).toStrictEqual('house');
+      expect(result.playlists).toBeDefined();
+      expect(result.playlists?.results.length).toBeGreaterThan(0);
+    });
+
+    it('search album should return albums', async () => {
+      const result = await sdk.search.search({
+        text: 'Meteora',
+        type: 'album',
+        page: 1,
+      });
+
+      expect(fetchSpy.request().input).toBe(
+        `https://api.music.yandex.net/search?text=Meteora&type=album&page=1`,
+      );
+
+      expect(result.type).toStrictEqual('album');
+      expect(result.page).toStrictEqual(1);
+      expect(result.text).toStrictEqual('Meteora');
+      expect(result.albums).toBeDefined();
+      expect(result.albums?.results.length).toBeGreaterThan(0);
+    });
   });
 
-  it('search track should return tracks', async () => {
-    const result = await sdk.search.search({
-      text: 'underdog',
-      type: 'track',
-      page: 1,
+  describe('search suggest', () => {
+    it('search suggest artist should return artists', async () => {
+      const result = await sdk.search.searchSuggest('Kasabia');
+
+      expect(fetchSpy.request().input).toBe(
+        `https://api.music.yandex.net/search/suggest?part=Kasabia`,
+      );
+
+      expect(result.best.text).toStrictEqual('kasabian');
+      expect(result.best.type).toStrictEqual('artist');
+      expect((result.best.result as Artist).name).toStrictEqual('Kasabian');
     });
-
-    expect(fetchSpy.request().input).toBe(
-      `https://api.music.yandex.net/search?text=underdog&type=track&page=1`,
-    );
-
-    expect(result.type).toStrictEqual('track');
-    expect(result.page).toStrictEqual(1);
-    expect(result.text).toStrictEqual('underdog');
-    expect(result.tracks).toBeDefined();
-    expect(result.tracks?.results.length).toBeGreaterThan(0);
-  });
-
-  it('search playlist should return playlists', async () => {
-    const result = await sdk.search.search({
-      text: 'house',
-      type: 'playlist',
-      page: 1,
-    });
-
-    expect(fetchSpy.request().input).toBe(
-      `https://api.music.yandex.net/search?text=house&type=playlist&page=1`,
-    );
-
-    expect(result.type).toStrictEqual('playlist');
-    expect(result.page).toStrictEqual(1);
-    expect(result.text).toStrictEqual('house');
-    expect(result.playlists).toBeDefined();
-    expect(result.playlists?.results.length).toBeGreaterThan(0);
-  });
-
-  it('search album should return albums', async () => {
-    const result = await sdk.search.search({
-      text: 'Meteora',
-      type: 'album',
-      page: 1,
-    });
-
-    expect(fetchSpy.request().input).toBe(
-      `https://api.music.yandex.net/search?text=Meteora&type=album&page=1`,
-    );
-
-    expect(result.type).toStrictEqual('album');
-    expect(result.page).toStrictEqual(1);
-    expect(result.text).toStrictEqual('Meteora');
-    expect(result.albums).toBeDefined();
-    expect(result.albums?.results.length).toBeGreaterThan(0);
   });
 });
