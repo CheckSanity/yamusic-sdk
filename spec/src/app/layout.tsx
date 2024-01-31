f72a9dd8ac5fd8e85bce3d0ede64b00b752147f0
Garik Bezruchko <garik.bezruchko@yandex.ru>
2024-01-31 22:32:00 +0300

    Update OpenApi spec website

--- spec/src/app/layout.tsx
+++ spec/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next'
-import StyledComponentsRegistry from '@/lib/registry'
 import React from 'react'
 
 export const metadata: Metadata = {
@@ -14,9 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
